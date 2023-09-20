73463488ea49858b465d5ab158a98b067242792b
Guillermo Tala <guille86598@gmail.com>
2023-09-20 03:50:11 -0300

    new version

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,11 @@
 import { configureChains, createConfig } from 'wagmi'
-import { sepolia, mainnet } from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 
 import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [sepolia] : [])],
+  [sepolia],
   [
     publicProvider(),
   ],
